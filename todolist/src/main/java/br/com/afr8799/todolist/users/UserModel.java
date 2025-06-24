2c6a03eb7badb81d3cb243d03d2d1c95e5d259df
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-06-23 23:11:34 -0300

    Error cannot find symbol UserController.java / UserModel ln 16

---
+++ todolist/src/main/java/br/com/afr8799/todolist/users/UserModel.java
@@ -0,0 +1,10 @@
+
+package br.com.afr8799.todolist.users;
+
+public class UserModel {
+    //Classe que irá receber criar o objeto contendo infromações do usuário 
+
+    String username;
+    String name;
+    String password;
+}
