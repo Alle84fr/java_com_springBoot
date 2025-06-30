4556e5d37f008bb1f099b821fd5d73f3f4ac68d1
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-06-30 14:30:43 -0300

    add config H2 add config spring.boot add aula 8 update aula 7 update names aulas

--- todolist/src/main/java/br/com/afr8799/todolist/users/UserModel.java
+++ todolist/src/main/java/br/com/afr8799/todolist/users/UserModel.java
@@ -1,6 +1,10 @@
 
 package br.com.afr8799.todolist.users;
 
+import lombok.Data;
+
+//define getter e setter juntos
+@Data
 public class UserModel {
     //Classe que irá receber criar o objeto contendo infromações do usuário 
 
@@ -8,32 +12,5 @@ public class UserModel {
     private String name;
     private String password;
 
-    // método para inserir valor ao user name, que é privado
-    public void setUsername(String username) {
-        this.username = username;
-    
-        // this.username está se referindo a linha private String username (ln 7)
-        // username é se referindo ao parâmetro String username - dado que virá
-    }
 
-    // para recuperar, buscar o dado
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword (String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
